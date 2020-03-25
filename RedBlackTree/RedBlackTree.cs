7bc04b9816fff71b557d2aed1e2b5c274452b572
BustaZ <pavel.zaborin@inbox.ru>
2020-03-25 13:24:26 +0500

    Make the main classes: Node, RedBlackTree and Colors. All without functionality.

---
+++ RedBlackTree/RedBlackTree.cs
@@ -0,0 +1,20 @@
+﻿namespace RedBlackTree
+{
+    public class RedBlackTree
+    {
+        private Node _root;
+
+        public RedBlackTree(int valueRoot)
+        {
+            _root = new Node(valueRoot);
+        }
+
+        public void InsertNode(int value){}
+        
+        public void DeleteNode(int value){}
+        
+        public void LeftRotation(Node node){}
+        
+        public void RightRotation(Node node){}
+    }
+}
