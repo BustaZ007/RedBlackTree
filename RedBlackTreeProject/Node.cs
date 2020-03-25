198e5e7ad10719fe1010f39ad7d2759d5defd8b4
BustaZ <pavel.zaborin@inbox.ru>
2020-03-25 20:41:31 +0500

    Add realisation of insert. Also add function of rotation (left and right). Some fix.

--- RedBlackTreeProject/Node.cs
+++ RedBlackTreeProject/Node.cs
@@ -3,47 +3,65 @@ using System;
 
 namespace RedBlackTree
 {
-    public class Node : ANode
+    public class Node 
     {
         private Color _color;
-        private readonly int _value;
-        private ANode _left;
-        private ANode _right;
+        private readonly int? _value;
+        private Node _left;
+        private Node _right;
         private Node _parent;
 
-        public Node(int value, Node parent = null) : base(parent)
+        public Node(int? value, Node parent = null)
         {
-            _color = Color.Red;
-            _left = new Nil(this);
-            _right = new Nil(this);
+            _color = value == null ? Color.Black : Color.Red;
+            _left = value == null ? null : new Node(null, this);
+            _right = value == null ? null : new Node(null, this);
             _parent = parent;
             _value = value;
         }
+        
+        public Node GetLeft() => _left;
 
-        public ANode GetLeft() => _left;
-
-        public ANode GetRight() => _right;
-
-        public override Color GetColor() => _color;
-        public override Node GetParent() => _parent;
-
-        public override bool IsNil() => false;
-
-        public void AddChild(int value)
+        public void SetLeft(Node left)
         {
-            if (value >= _value)
-            {
-                _right = new Node(value,this);
-            }
-            else
-            {
-                _left = new Node(value,this);
-            }
+            _left = left;
         }
 
+        public Node GetRight() => _right;
+        
+        public void SetRight(Node right)
+        {
+            _right = right;
+        }
+
+        public Color GetColor() => _color;
         public void Repaint()
         {
             _color = _color == Color.Black ? Color.Red : Color.Black;
         }
+
+        public void SetColor(Color color)
+        {
+            _color = color;
+        }
+        public Node GetParent() => _parent;
+
+        public void SetParent(Node parent)
+        {
+            _parent = parent;
+        }
+
+        public bool IsNil() => _value == null;
+
+        public int? GetValue() => _value;
+
+        public void AddChild(int value)
+        {
+            if (value >= _value)
+                _right = new Node(value, this);
+            else
+                _left = new Node(value, this);
+        }
+
     }
 }
--- RedBlackTreeProject/RedBlackTree.cs
+++ RedBlackTreeProject/RedBlackTree.cs
@@ -1,4 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
 
 namespace RedBlackTree
 {
@@ -11,11 +14,33 @@ namespace RedBlackTree
         public RedBlackTree(int valueRoot)
         {
             _root = new Node(valueRoot);
+            _root.Repaint();
             _allNodes.Add(_root);
         }
 
         public bool InsertNode(int value)
         {
+            if (_allNodes.Select(n => n.GetValue()).Contains(value))
+                return false;
+            Node currentNode = _root;
+            while (!currentNode.IsNil())
+            {
+                if (value > currentNode.GetValue())
+                {
+                    currentNode = currentNode.GetRight();
+                }
+                else
+                {
+                    currentNode = currentNode.GetLeft();
+                }
+            }
+            currentNode.GetParent().AddChild(value);
+            Node newNode = currentNode.GetParent().GetLeft().GetValue() == value
+                ? currentNode.GetParent().GetLeft()
+                : currentNode.GetParent().GetRight();
+            _allNodes.Add(newNode);
+            InsertCase1(newNode);
+            
             return true;
         }
 
@@ -24,10 +49,230 @@ namespace RedBlackTree
             return true;
             
         }
+
+        private void InsertCase1(Node node)
+        {
+            if(node.GetParent() == null)
+                node.Repaint();
+            else
+                InsertCase2(node);
+        }
+
+        private void InsertCase2(Node node)
+        {
+            if (node.GetParent().GetColor() == Color.Black)
+                return;
+            InsertCase3(node);
+        }
+
+        private void InsertCase3(Node node)
+        {
+            Node uncle = GetUncle(node);
+            if (uncle != null && uncle.GetColor() == Color.Red)
+            {
+                node.GetParent().Repaint();
+                uncle.Repaint();
+                Node g = GetGrandparent(node);
+                g.Repaint();
+                InsertCase1(g);
+            }
+            else
+                InsertCase4(node);
+        }
+
+        private void InsertCase4(Node node)
+        {
+            Node g = GetGrandparent(node);
+            if (node == node.GetParent().GetRight() && node.GetParent() == g.GetLeft())
+            {
+                LeftRotation(node.GetParent());
+                node = node.GetLeft();
+            }
+            else if (node == node.GetParent().GetLeft() &&  node.GetParent() == g.GetRight())
+            {
+                RightRotation(node.GetParent());
+                node = node.GetRight();
+            }
+            InsertCase5(node);
+        }
+
+        private void InsertCase5(Node node)
+        {
+            Node g = GetGrandparent(node);
+            node.GetParent().Repaint();
+            g.Repaint();
+            if(node == node.GetParent().GetLeft() && node.GetParent() == g.GetLeft())
+                RightRotation(g);
+            else
+                LeftRotation(g);
+        }
         
-        public void LeftRotation(Node node){}
+        private Node GetGrandparent(Node node) =>
+            node != null && node.GetParent() != null
+                ? node.GetParent().GetParent()
+                : null;
+
+        private Node GetUncle(Node node)
+        {
+            Node g = GetGrandparent(node);
+            if (g == null)
+                return null;
+            return node.GetParent() == g.GetLeft()
+                ? g.GetRight()
+                : g.GetLeft();
+        }
+
+        private Node GetBrother(Node node) =>
+            node == node.GetParent().GetLeft()
+                ? node.GetParent().GetRight()
+                : node.GetParent().GetLeft();
+
+        private void ReplaceNode(Node node, Node child)
+        {
+            child.SetParent(node.GetParent());
+            if(node == node.GetParent().GetLeft())
+                node.GetParent().SetLeft(child);
+            else
+                node.GetParent().SetRight(child);
+        }
+
+        private void DeleteOneChild(Node node)
+        {
+            Node child = node.GetRight().IsNil() ? node.GetLeft() : node.GetRight();
+            ReplaceNode(node, child);
+            if (node.GetColor() == Color.Black)
+                if (child.GetColor() == Color.Red)
+                    child.Repaint();
+                else
+                    DeleteCase1(child);
+        }
+
+        private void DeleteCase1(Node node)
+        {
+            if (node.GetParent() == null)
+                DeleteCase2(node);
+        }
         
-        public void RightRotation(Node node){}
+        private void DeleteCase2(Node node)
+        {
+            Node s = GetBrother(node);
+
+            if (s.GetColor() == Color.Red)
+            {
+                node.GetParent().Repaint();
+                s.Repaint();
+                if(node == node.GetParent().GetLeft())
+                    LeftRotation(node.GetParent());
+                else
+                    RightRotation(node.GetParent());
+            }
+            DeleteCase3(node);
+        }
+        
+        private void DeleteCase3(Node node)
+        {
+            Node s = GetBrother(node);
+
+            if (node.GetParent().GetColor() == Color.Black && s.GetColor() == Color.Black &&
+                s.GetLeft().GetColor() == Color.Black && s.GetRight().GetColor() == Color.Black)
+            {
+                s.Repaint();
+                DeleteCase1(node.GetParent());
+            }
+            else
+                DeleteCase4(node);
+        }
+        
+        private void DeleteCase4(Node node)
+        {
+            Node s = GetBrother(node);
+
+            if (node.GetParent().GetColor() == Color.Red && s.GetColor() == Color.Black &&
+                s.GetLeft().GetColor() == Color.Black && s.GetRight().GetColor() == Color.Black)
+            {
+                s.Repaint();
+                node.GetParent().Repaint();
+            }
+            else
+                DeleteCase5(node);
+        }
+        
+        private void DeleteCase5(Node node)
+        {
+            Node s = GetBrother(node);
+
+            if (s.GetColor() == Color.Black)
+            {
+                if (node == node.GetParent().GetLeft() && s.GetRight().GetColor() == Color.Black &&
+                    s.GetLeft().GetColor() == Color.Red)
+                {
+                    s.Repaint();
+                    s.GetLeft().Repaint();
+                    RightRotation(s);
+                }
+                else if (node == node.GetParent().GetRight() && s.GetLeft().GetColor() == Color.Black &&
+                         s.GetRight().GetColor() == Color.Red)
+                {
+                    s.Repaint();
+                    s.GetRight().Repaint();
+                    LeftRotation(s);
+                }
+            }
+            DeleteCase6(node);
+        }
+        
+        private void DeleteCase6(Node node)
+        {
+            Node s = GetBrother(node);
+            
+            s.SetColor(node.GetParent().GetColor());
+            node.GetParent().SetColor(Color.Black);
+            if (node == node.GetParent().GetLeft())
+            {
+                s.GetRight().SetColor(Color.Black);
+                LeftRotation(node.GetParent());
+            }
+            else
+            {
+                s.GetLeft().SetColor(Color.Black);
+                RightRotation(node.GetParent());
+            }
+        }
+        public void LeftRotation(Node node)
+        {
+            Node pivot = node.GetRight();
+            pivot.SetParent(node.GetParent());
+            if (node.GetParent() != null)
+                if(node.GetParent().GetLeft() == node)
+                    node.GetParent().SetLeft(pivot);
+                else
+                    node.GetParent().SetRight(pivot);
+            
+            node.SetRight(pivot.GetLeft());
+            if(pivot.GetLeft() != null)
+                pivot.GetLeft().SetParent(node);
+            
+            node.SetParent(pivot);
+            pivot.SetLeft(node);
+        }
+
+        public void RightRotation(Node node)
+        {
+            Node pivot = node.GetLeft();
+            pivot.SetParent(node.GetParent());
+            if (node.GetParent() != null)
+                if(node.GetParent().GetLeft() == node)
+                    node.GetParent().SetLeft(pivot);
+                else
+                    node.GetParent().SetRight(pivot);
+            
+            node.SetLeft(pivot.GetRight());
+            if(pivot.GetRight() != null)
+                pivot.GetRight().SetParent(node);
+            
+            node.SetParent(pivot);
+            pivot.SetRight(node);
+        }
 
         public Node GetRoot() => _root;
 
