c4f8688718ecced8da312b20d33cb49f09f4c9ff
unknown <pavel.zaborin@inbox.ru>
2020-03-25 12:35:30 +0500

    Make projects

---
+++ RedBlackTreeTests/RedBlackTreeTest1.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RedBlackTree;
+
+namespace RedBlackTreeTests
+{
+    [TestClass]
+    public class RedBlackTreeTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
