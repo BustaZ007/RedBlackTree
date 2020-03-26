c0927b1fb034b77f43469d3d2a1faa65c21ac983
BustaZ <pavel.zaborin@inbox.ru>
2020-03-26 11:55:43 +0500

    Add two test. Deleting root situation.

--- RedBlackTreeTests/RedBlackTreeTest1.cs
+++ RedBlackTreeTests/RedBlackTreeTest1.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel;
 using System.Drawing;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using RedBlackTree;
@@ -17,6 +18,7 @@ namespace RedBlackTreeTests
             tree.InsertNode(17);
             Assert.AreEqual(3, tree.GetAllNodes().Count);
         }
+
         [TestMethod]
         public void CheckRedNodesAfterInsert()
         {
@@ -35,9 +37,11 @@ namespace RedBlackTreeTests
                     res = node.GetLeft().IsNil() && node.GetRight().IsNil() ||
                           node.GetRight().GetColor() == Color.Black && node.GetLeft().GetColor() == Color.Black;
                 }
+
                 Assert.AreEqual(true, res, "Red parent can have only black child");
             }
         }
+
         [TestMethod]
         public void CheckForNodeExistenceWhenInserting()
         {
@@ -47,7 +51,7 @@ namespace RedBlackTreeTests
             Assert.AreEqual(true, tree.InsertNode(5));
             Assert.AreEqual(false, tree.InsertNode(8));
         }
-        
+
         [TestMethod]
         public void CheckLenghtListOfNodesAfterDelete()
         {
@@ -57,7 +61,7 @@ namespace RedBlackTreeTests
             tree.DeleteNode(8);
             Assert.AreEqual(2, tree.GetAllNodes().Count);
         }
-        
+
         [TestMethod]
         public void CheckRedNodesAfterDelete()
         {
@@ -79,10 +83,11 @@ namespace RedBlackTreeTests
                     res = node.GetLeft().IsNil() && node.GetRight().IsNil() ||
                           node.GetRight().GetColor() == Color.Black && node.GetLeft().GetColor() == Color.Black;
                 }
+
                 Assert.AreEqual(true, res, "Red parent can have only black child");
             }
         }
-        
+
         [TestMethod]
         public void CheckForNodeExistenceWhenDeleting()
         {
@@ -91,6 +96,39 @@ namespace RedBlackTreeTests
             tree.InsertNode(17);
             Assert.AreEqual(false, tree.DeleteNode(5));
             Assert.AreEqual(true, tree.DeleteNode(8));
+        }
+
+        [TestMethod]
+        public void DeleteOneNodeRoot()
+        {
+            RedBlackTree.RedBlackTree tree = new RedBlackTree.RedBlackTree(12);
+            tree.DeleteNode(12);
+            Assert.AreEqual(false, tree.DeleteNode(11));
+            Assert.AreEqual(true, tree.InsertNode(14));
+            Assert.AreEqual(1, tree.GetAllNodes().Count);
+        }
+
+        [TestMethod]
+        public void InsertAfterDeleteRoot()
+        {
+            RedBlackTree.RedBlackTree tree = new RedBlackTree.RedBlackTree(12);
+            tree.InsertNode(8);
+            tree.InsertNode(17);
+            tree.InsertNode(1);
+            tree.DeleteNode(12);
+            tree.InsertNode(11);
+            tree.InsertNode(15);
+            tree.InsertNode(25);
+            foreach (var node in tree.GetAllNodes())
+            {
+                var res = true;
+                if (node.GetColor() == Color.Red)
+                {
+                    res = node.GetLeft().IsNil() && node.GetRight().IsNil() ||
+                          node.GetRight().GetColor() == Color.Black && node.GetLeft().GetColor() == Color.Black;
+                }
+                Assert.AreEqual(true, res, "Red parent can have only black child");
+            }
         }
     }
 }
